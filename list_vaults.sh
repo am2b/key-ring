163df1f3699441226f668733d7ac507adaea583c
am2b <20837051+am2b@users.noreply.github.com>
2024-12-27 00:14:43 +0800

    finish:list_vaults.sh

---
+++ list_vaults.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+#=vault
+#@list all vaults in the keyring directory
+#@usage:
+#@list_vaults.sh
+
+SELF_ABS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+source "${SELF_ABS_DIR}"/shared.sh
+
+usage() {
+    local script
+    script=$(basename "$0")
+    echo "usage:"
+    echo "$script"
+    exit 1
+}
+
+check_parameters() {
+    if (("$#" != 0)); then
+        usage
+    fi
+}
+
+process_opts() {
+    while getopts ":h" opt; do
+        case $opt in
+        h)
+            usage
+            ;;
+        *)
+            echo "error:unsupported option -$opt"
+            usage
+            ;;
+        esac
+    done
+}
+
+list_vaults() {
+    local keyring_dir
+    keyring_dir=$(get_keyring_dir)
+
+    local vaults
+    vaults=($(find "${keyring_dir}" -mindepth 1 -type d))
+
+    for v in "${vaults[@]}"; do
+        echo "${v}"
+    done
+}
+
+main() {
+    check_parameters "${@}"
+    process_opts "${@}"
+    shift $((OPTIND - 1))
+
+    list_vaults
+}
+
+main "${@}"
